767faee64460fde0dd4d39e9ff3548ced413fa28
Nikolaos Karmperis <karmperis@gmail.com>
2026-04-13 20:28:19 +0300

    Implement user model, routes, and service; set up database connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import app from './app';
+import {connectDB} from './utils/db';
+
+const start = async() =>{
+  await connectDB();
 
 app.listen(5068, ()=> {
   console.log("Server is up");
 })
+}
+
+start();
