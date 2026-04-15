186c30f9b758703e7a6fc32f063d8fb4fbd2d9a1
Nikolaos Karmperis <karmperis@gmail.com>
2026-04-15 20:59:45 +0300

    feat: add updateUser method for updating user details by username

--- src/dao/user.dao.ts
+++ src/dao/user.dao.ts
@@ -8,3 +8,7 @@ export const createUser = async (data: Partial<IUser>):Promise<IUser> => {
   const user = new User(data);
   return await user.save();
 }
+
+export const updateUser = async(username: string, payload: Partial<IUser>) :Promise<IUser | null> => {  
+  return await User.findOneAndUpdate({username: username}, payload, {new: true}).populate('roles').lean().exec();
+}
