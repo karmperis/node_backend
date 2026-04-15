c752f4038d54699722e86aa651392971aee8678b
Nikolaos Karmperis <karmperis@gmail.com>
2026-04-15 20:47:00 +0300

    feat: refactor createUser service to handle password hashing and role assignment

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,41 @@
 import User, { IUser } from '../models/user.model';
 import Role, { IRole } from '../models/role.model';
-
 import { Types } from 'mongoose';
+import bcrypt from 'bcrypt';
+import { CreateUserDto } from '../dto/user.dto';
+import * as userDAO from '../dao/user.dao';
 
-export const createUser = async(payload:IUser) => {
-  console.log("Service Payload", payload);
-  const user = new User({
-    "username":payload.username,
-    "password":payload.password
-  });
-  console.log("New user: ", user);
-  return user.save();
+export const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS || '10');
+
+export const findUserByEmail = async(email:string) => {
+  const user = await userDAO.findByEmail(email);
+  if (user) {
+    return user
+  }
 }
+
+export const createUser = async(payload:CreateUserDto) => {
+  if  (payload.password){
+    const hash = await bcrypt.hash(payload.password, SALT_ROUNDS);
+    payload.password = hash;
+  }
+
+  let roleIds: Types.ObjectId[] = [];
+  if (payload.roles && payload.roles.length > 0) {
+    roleIds = payload.roles.map(id => new Types.ObjectId(id));
+  } else {
+    let reader: IRole | null = await Role.findOne({role: "READER"});
+    if (!reader) {
+      reader = await Role.create({role: 'READER', description: "Default Role", active: true});
+    }
+    roleIds = [reader._id];
+  }
+
+  const user = await userDAO.createUser({
+    ...payload,
+    roles: roleIds
+  });
+
+  return user
+}
+
