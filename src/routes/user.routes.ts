25d5b2b7a59767f2cec632616cc62ad3a35c25d8
Nikolaos Karmperis <karmperis@gmail.com>
2026-04-18 00:22:04 +0300

    feat: add authentication and validation middleware to user creation route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import * as userCtrl from '../controller/user.controller';
+import { authenticate } from '../middlewares/auth.middleware';
+import { hasReaderRole } from '../middlewares/user.middleware';
+import { validate } from '../middlewares/validate.middleware';
+import { createUserSchema, updateUserSchema } from '../validators/user.validator';
 
 const router = Router();
 
 router.get('/', userCtrl.getAll);
 router.get('/email/:email', userCtrl.getOneByEmail);
-router.post('/', userCtrl.create);
+router.post('/',authenticate, hasReaderRole, validate(createUserSchema), userCtrl.create);
 router.put('/:username', userCtrl.update);
 
 export default router;
